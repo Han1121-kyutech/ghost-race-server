d159b6647d6e766b84a3efffd3133d0b1ef48f70
Han <chohan161121@mail.kyutech.jp>
2026-02-13 04:32:29 +0900

    カメラ追加(未修正)

---
+++ Assets/Scripts/Camera/MouseLook.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f; // マウスの感度
+    public Transform playerBody;         // プレイヤー本体のTransform
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        if (GameManager.instance.start == true)
+        {
+            // マウスカーソルを画面中央に固定して消す（ゲーム中に邪魔にならないように）
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    void Update()
+    {
+        if (GameManager.instance.start == true)
+        {
+
+            // マウスの入力を取得
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            // 上下の回転（カメラだけを上下に動かす）
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f); // 真上や真後ろを見すぎないように制限
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+            // 左右の回転（プレイヤー本体ごと回転させる）
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
